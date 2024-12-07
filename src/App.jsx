f3a25f97be85b6a6d676a1331faee608f95c7c20
MR.BeN <1mrben1@duck.com>
2024-12-07 22:36:24 +0200

    Fixed import path for Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Services from "./components/Services";
 import Team from "./components/Team";
 import Contact from "./components/Contact";
 import Blog from "./components/Blog";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 
 function App() {
   // add AOS
